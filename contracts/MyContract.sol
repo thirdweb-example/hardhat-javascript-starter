9ed6e13b88df1cbc68e5e7429ea7cfb53a5602ce
adam-maj <mr.adam.maj@gmail.com>
2022-07-29 13:53:59 -0700

    Add constructor and remove dependencies

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.9;
 
 contract MyContract {
-
+    constructor() {}
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,3 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
