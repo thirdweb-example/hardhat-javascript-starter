c72f63ab339b033a7932502ee96a155b098e3771
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2022-08-03 07:23:47 +1000

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,12 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
