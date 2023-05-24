8e56469ccea2fa2d11fa2b87a148f58914c2eac7
Yash <kumaryashcse@gmail.com>
2023-05-25 03:50:00 +0530

    add mainnet config

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,7 +19,14 @@ module.exports = {
   networks: {
     zksync_testnet: {
       url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli",
       chainId: 280,
+      zksync: true,
+    },
+    zksync_mainnet: {
+      url: "https://zksync2-mainnet.zksync.io/",
+      ethNetwork: "mainnet",
+      chainId: 324,
       zksync: true,
     },
   },
